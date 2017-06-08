3c206dba9b4e17d3bc3f81a9dbf7108f637eeb78
László Kardinál <laszlo.kardinal@digitalnatives.hu>
2017-06-08 18:17:32 +0200

    Installed apollo

--- client/index.jsx
+++ client/index.jsx
@@ -10,5 +10,24 @@ import App from "./components/App.jsx";
 import "react-mdl/extra/material.css";
 import "react-mdl/extra/material.js";
 
-ReactDOM.render( <App />, document.getElementById( "react-root" ) );
+import {
+  ApolloClient,
+  createNetworkInterface,
+  ApolloProvider
+} from 'react-apollo';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://api.example.com/graphql'
+});
+
+const client = new ApolloClient({
+  networkInterface: networkInterface
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById( "react-root" )
+);
 
