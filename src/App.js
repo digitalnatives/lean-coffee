50e4865206497eec2e1d2ee49c7dbc5a7b2da024
Muriel Silveira <murielkong@gmail.com>
2017-07-08 20:26:25 +0200

    connect with any graphql api until I have the correct url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { gql, graphql } from 'react-apollo'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
+    const { loading, allPosts } = this.props.data
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-content">
+          {
+            loading ? 'Loading'
+            : allPosts.map(post => (
+              <div className="Post" key={post.id}>
+                <img
+                  src={post.imageUrl}
+                  alt={post.description}
+                  style={{
+                    width: '600px',
+                    maxHeight: '400px',
+                    objectFit: 'cover',
+                  }}
+                />
+                <div>{post.description}</div>
+              </div>
+            ))
+          }
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const ListQuery = gql`
+  query allPosts {
+    allPosts(orderBy: createdAt_DESC) {
+      id
+      description
+      imageUrl
+    }
+  }
+`
+
+export default graphql(ListQuery)(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createNetworkInterface, ApolloClient, ApolloProvider } from 'react-apollo'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const networkInterface = createNetworkInterface({
+    uri: 'https://api.graph.cool/simple/v1/cj4vib6mwvqm50131ovkzqqht'
+})
+const client = new ApolloClient({ networkInterface })
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
+registerServiceWorker()
